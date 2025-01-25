97f10292deffcf26dffb6cb6236ba5fd48fcbc45
jkancc111 <jkancc3@gmail.com>
2025-01-25 17:12:25 +0700

    Add [2x🍀]Anime Slashing Simulator via Discord Bot

--- JC3PIHETOl7sVP0yJyDQ.lua
+++ JC3PIHETOl7sVP0yJyDQ.lua
@@ -111,6 +111,8 @@ local function createGameButton(placeName, placeId, scriptUrl)
 end
 
 -- Create buttons for each game
+createGameButton("[2x🍀]Anime Slashing Simulator", 18956736354, "https://raw.githubusercontent.com/jkancc111/Games-V2/main/AnimeSlashSim.txt")
+
 createGameButton("[UPD🦑] Car Training🚗", 111130915266245, "https://raw.githubusercontent.com/jkancc111/Games-V2/main/CarTraining.txt")
 
 createGameButton("[🎉RELEASE] Pet Star Simulator! 🐾", 86430667919924, "https://raw.githubusercontent.com/jkancc111/Games-V2/main/StarPetSimulator.lua")
